11b73db8dd0d741cdb59bf2eefe12bcc0de79202
Vadasz Laszlo <maxinteger@gmail.com>
2013-01-17 17:53:28 +0100

    settings update

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -1,6 +1,5 @@
 # Django settings for ribbit project.
 import os
-import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -23,7 +22,7 @@ DATABASES = {
 }
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
